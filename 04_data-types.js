24e39641a31714e1a659b528fd496e87263c1bfe
Mostafizur Rahman <mostafizzfuad@gmail.com>
2025-05-08 20:17:03 +0600

    data types - number added

---
+++ 04_data-types.js
@@ -0,0 +1,103 @@
+// Data types represent the different kinds of values we can use in JavaScript.
+
+/*********************** 
+JavaScript data types are divided into primitive and non-primitive types.
+
+Primitive Data Types : They can hold a single simple value. ( Defined By Javascript )
+    String
+    Number 
+    BigInt
+    Boolean
+    undefined
+    null
+    Symbol
+
+Non-Primitive Data Types : They can hold multiple values. ( Defined By The user )
+Object
+    Object
+    Array
+    Date
+***********************/
+
+// ==========  Number  ========== //
+let id = 247; // number literal (int)
+let age = 25.5; // number literal (float)
+let num = Number(56); // number constructor
+
+console.log(id); // 247
+console.log(age); // 25.5
+console.log(num); // 56
+
+// ==========  convert string to number  ========== //
+console.log(Number("32")); // 32
+console.log(typeof Number("32")); // number
+
+console.log(Number.parseInt("3.41")); // 3
+console.log(Number.parseFloat("3.41")); // 3.41
+
+console.log(Number.parseInt(75.88)); // 75 (convert float to integer)
+
+// ==========  convert boolean to number  ========== //
+console.log(Number(true)); // 1
+console.log(Number(false)); // 0
+
+// ==========  convert number to string  ========== //
+console.log(typeof String(247)); // string
+console.log(typeof toString(247)); // string ⇒ old system
+
+// ==========  max and min value  ========== //
+console.log(Number.MAX_VALUE); // max integer value
+console.log(Number.MIN_SAFE_INTEGER); // min integer value
+
+// ==========  Infinity  ========== //
+console.log(1 / 0); // Infinity
+console.log(50 / 0); // Infinity
+console.log(-52 / 0); // -Infinity
+console.log(typeof Infinity); // number
+
+// ==========  NaN  ========== //
+const result = 4 - "hello";
+console.log(result); // NaN
+console.log(typeof NaN); // number
+
+const countryName = "Bangladesh";
+const converted = Number(countryName);
+console.log(converted); // NaN
+
+console.log("abc" * 10); // NaN -> Not a number
+console.log(typeof NaN); // number
+
+// ==========  isNaN  ========== //
+console.log(6 - 10); // -4
+console.log(isNaN(6 - 10)); // false
+console.log(isNaN("6 - 10")); // true
+console.log();
+console.log("6" - "10"); // -4
+console.log(isNaN("6" - "10")); // false
+console.log(isNaN("hello")); // true
+console.log(isNaN(Infinity)); // false
+console.log(isNaN(-Infinity)); // false
+console.log(isNaN(true)); // false
+
+// =========  convert hexadecimal to decimal  ========== //
+let hex = 0xff;
+console.log(hex); // 255
+
+// =========  convert octal to decimal  ========== //
+let oct = 0o756;
+console.log(oct); // 494
+
+// =========  convert binary to decimal  ========== //
+let bin = 0b11010;
+console.log(bin); // 26
+
+// =========  toFixed() and toPrecision()  ========== //
+let number = 32.36578;
+
+// toFixed()
+console.log(number.toFixed(2)); // 32.37
+console.log(typeof number.toFixed(2)); // string
+
+// toPrecision()
+console.log(number.toPrecision(5)); // 32.366
+console.log(typeof number.toPrecision(5)); // string
